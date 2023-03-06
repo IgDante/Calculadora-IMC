8ce63950d4c41b985186bad66309e10461143ac9
Dante <danteigor424@gmail.com>
2023-03-05 22:41:57 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -14,22 +14,40 @@ btn.addEventListener('click', (e) => {
 
 });
 
+document.addEventListener("keypress", function(e) {
+    if(e.key === 'Enter') {
+    
+        var btn = document.querySelector("#submit");
+      
+      btn.click();
+    }
+  });
+
 function imc(weight, height) {
     let imc = weight / (height * height);
+    const resultIMC = document.querySelector('#result-imc')
+    const result = document.querySelector('#result');
 
-    console.log(imc);
     
-    if (imc < 18.5) {
-        console.log('Abaixo do peso')
-    } else if (imc >= 18.5 && imc < 25.0) {
-        console.log('Peso ideal')
-    } else if (imc >= 25.0 && imc < 30.0) {
-        console.log('Acima do peso')
-    } else if (imc >= 30.0 && imc < 35.0) {
-        console.log('Obesidade grau I')
-    } else if (imc >= 35.0 && imc < 40.0) {
-        console.log('Obesidade grau II')
-    } else if (imc >= 40.0) {
-        console.log('Obesidade grau III')
-    }
-}
+    if(weight != 0 && height != 0) {
+        resultIMC.innerHTML = imc.toFixed(2);
+
+        if (imc < 18.5) {
+            result.innerHTML = 'Abaixo do peso'
+        } else if (imc >= 18.5 && imc < 25.0) {
+            result.innerHTML = 'Peso ideal' 
+        } else if (imc >= 25.0 && imc < 30.0) {
+            result.innerHTML = 'Acima do peso';
+        } else if (imc >= 30.0 && imc < 35.0) {
+            result.innerHTML = 'Obesidade grau I';
+        } else if (imc >= 35.0 && imc < 40.0) {
+            result.innerHTML = 'Obesidade grau II';
+        } else if (imc >= 40.0) {
+            result.innerHTML = 'Obesidade grau III';
+        }
+    } else {
+        resultIMC.innerHTML = '';
+        result.innerHTML = '';
+        result.innerHTML = 'Digite o peso e altura';    
+    };  
+};
